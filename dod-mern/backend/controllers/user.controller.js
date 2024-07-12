a474e5fc6026b58ecefece0fa6607eb30a41e8d3
leocodeio <basicleo@787>
2024-07-12 23:06:09 +0530

    added login in backend

--- dod-mern/backend/controllers/user.controller.js
+++ dod-mern/backend/controllers/user.controller.js
@@ -1,20 +1,38 @@
 import User from "../models/user.model.js";
 
-const signup = async(req,res)=>{
-  try{
-    const {userName,password,domainName,url}=req.body;
+const signup = async (req, res) => {
+  try {
+    const { userName, password, domainName, url } = req.body;
 
-    const exist=await User.findOne({where:{userName:userName}});
-    if(exist){
-      return res.status(400).json({message:"username already taken"})
+    const exist = await User.findOne({ where: { userName: userName } });
+    if (exist) {
+      return res.status(400).json({ message: "Username already taken" });
     }
-    const user = await User.create({userName,password,domainName,url});
-    res.status(200).json({user});
-  }catch(err){
-    res.status(500).send("error while signing up",err)
-    console.error("error while signing up",err);
+
+    const user = await User.create({ userName,password, domainName, url });
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ message: "Error while signing up" });
+    console.error("Error while signing up", err);
   }
-}
+};
 
-export {signup};
+const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const exist = await User.findOne({ where: { userName: userName } });
+    if (exist) {
+      if (password===exist.password) {
+        return res.status(200).json({ message: "User logged in successfully" });
+      } else {
+        return res.status(400).json({ message: "Wrong password" });
+      }
+    } else {
+      return res.status(400).json({ message: "Username does not exist" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: "Internal error while logging in" });
+  }
+};
 
+export { signup, login };
